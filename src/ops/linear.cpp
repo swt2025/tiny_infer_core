0c80c5da483f255451b4fd69f80f53bbfc96f099
swt2025 <2949657784@qq.com>
2026-08-01 14:25:29 +0800

    Add Linear op with tests

--- src/ops/linear.cpp
+++ src/ops/linear.cpp
@@ -1,33 +1,36 @@
+#include "tinyinfer/ops/linear.h"
+
 #include <cstddef>
 #include <stdexcept>
-#include "tinyinfer/ops/linear.h"
+
 #include "tinyinfer/ops/gemm.h"
 
 namespace tinyinfer {
-    Matrix Linear(const Matrix& input, const Matrix& weight, const Matrix& bias) {
-        std::size_t input_rows = input.rows();
-        std::size_t input_cols = input.cols();
-        std::size_t weight_rows = weight.rows();
-        std::size_t weight_cols = weight.cols();
-        std::size_t bias_rows = bias.rows();
-        std::size_t bias_cols = bias.cols();
 
-        if (input_rows != 1 || bias_rows != 1) {
-            throw std::invalid_argument("input and bias must be a vector!");
-        }
-
-        if (input_cols != weight_rows || weight_cols != bias_cols) {
-            throw std::invalid_argument("dimension not match!");
-        }
-        
-        Matrix output = Gemm(input, weight);
-
-        for (std::size_t row = 0; row < bias_rows; ++row) {
-            for (std::size_t col = 0; col < bias_cols; ++col) {
-                output(row, col) += bias(row, col);
-            }
-        }
-
-        return output;
+Matrix Linear(const Matrix& input, const Matrix& weight, const Matrix& bias) {
+    if (input.rows() == 0 || input.cols() == 0 ||
+        weight.rows() == 0 || weight.cols() == 0 ||
+        bias.rows() == 0 || bias.cols() == 0) {
+        throw std::invalid_argument("Linear input matrices must not be empty.");
     }
+
+    if (input.cols() != weight.rows()) {
+        throw std::invalid_argument("Linear input and weight shapes are not compatible.");
+    }
+
+    if (bias.rows() != 1 || bias.cols() != weight.cols()) {
+        throw std::invalid_argument("Linear bias shape must be [1, out_features].");
+    }
+
+    Matrix output = Gemm(input, weight);
+
+    for (std::size_t row = 0; row < output.rows(); ++row) {
+        for (std::size_t col = 0; col < output.cols(); ++col) {
+            output(row, col) += bias(0, col);
+        }
+    }
+
+    return output;
 }
+
+}  // namespace tinyinfer
